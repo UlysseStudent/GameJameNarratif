bc1e899fc5dd80e658a97134e306a7ff1e130682
Mauffret <u.mauffret@student.isartdigital.com>
2024-03-30 22:03:16 +0100

    changes

--- Assets/Scripts/ChoiceManager.cs
+++ Assets/Scripts/ChoiceManager.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class ChoiceManager : MonoBehaviour
+public class ChoiceManager : MonoBehaviour, IPointerEnterHandler,IPointerExitHandler
 {
     public TextMeshProUGUI text;
     public Choice choiceData;
     public TableauManager tableauManager;
     public Button button;
+    Animator animator;
+    AudioSource audioSource;
+    
     
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GameObject.Find("AudioManager").GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
         text.text = choiceData.text;
         
 
@@ -30,13 +35,21 @@ public class ChoiceManager : MonoBehaviour
 
    public void OnClick()
     {
+        audioSource.Play();
         TableauManager.unique.ChangeTableau(choiceData.goToTableau);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        animator.SetBool("Hover", true);
         
     }
 
-    
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        animator.Play("Iddle", 0, 0f);
+        animator.SetBool("Hover", false);
+        
 
-   
-
-    
+    }
 }
